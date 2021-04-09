bd247a348380f63d362c5227331e58d93a03f7fe
Mark Gritter <mgritter@akitasoftware.com>
2021-04-08 19:07:33 -0500

    Reconstruct timing information from HAR attributes
    There does not seem to be good way, as the code is currently
    written, to pass through the information in "Timings" directly.

--- apispec/har.go
+++ apispec/har.go
@@ -1,12 +1,14 @@
 package apispec
 
 import (
+	"time"
+
 	"github.com/google/martian/v3/har"
 	"github.com/google/uuid"
 	"github.com/pkg/errors"
 
-	"github.com/akitasoftware/akita-cli/printer"
 	hl "github.com/akitasoftware/akita-cli/har_loader"
+	"github.com/akitasoftware/akita-cli/printer"
 	"github.com/akitasoftware/akita-cli/trace"
 	"github.com/akitasoftware/akita-libs/akinet"
 	"github.com/akitasoftware/akita-libs/sampled_err"
@@ -39,6 +41,67 @@ func processHAR(inboundCol, outboundCol trace.Collector, p string) error {
 	return nil
 }
 
+// ReconstructedTimestamps holds times we can use to fill in
+// a ParsedNetworkTraffic, in such a way that the reported
+// latencies in the Witness will end up the same as in the HAR
+// file.  (Because I couldn't think of a good way to pass
+// the measurements through the to two separate Process
+// calls, without major refactoring.)
+//
+// In the future, we could save these timestamps directly in a
+// custom field, or pass the interval values through.
+type ReconstructedTimestamps struct {
+	RequestStart  time.Time
+	RequestEnd    time.Time
+	ResponseStart time.Time
+	ResponseEnd   time.Time
+}
+
+func advanceTime(current time.Time, ms *float32) time.Time {
+	if ms == nil || *ms <= 0.0 {
+		return current
+	}
+	// convert ms to nanoseconds
+	duration := time.Duration(int64(*ms * 1_000_000.0))
+	return current.Add(duration)
+}
+
+func (t *ReconstructedTimestamps) reconstructFromHAR(entry *hl.CustomHAREntry) {
+	currTime := entry.StartedDateTime
+	if currTime.IsZero() {
+		// Leave in all-zero state
+		return
+	}
+
+	if entry.Timings == nil {
+		// Mark them all with the timestamp we have
+		t.RequestStart = entry.StartedDateTime
+		t.RequestEnd = entry.StartedDateTime
+		t.ResponseStart = entry.StartedDateTime
+		t.ResponseEnd = entry.StartedDateTime
+		return
+	}
+
+	// Pre-SYN activities
+	currTime = advanceTime(currTime, entry.Timings.Blocked)
+	currTime = advanceTime(currTime, entry.Timings.DNS)
+
+	// SYN, handshake, and HTTP request
+	t.RequestStart = currTime
+	currTime = advanceTime(currTime, entry.Timings.Connect)
+	currTime = advanceTime(currTime, entry.Timings.SSL)
+	currTime = advanceTime(currTime, entry.Timings.Send)
+	t.RequestEnd = currTime
+
+	// Server processing time, before first byte of response
+	currTime = advanceTime(currTime, entry.Timings.Wait)
+	t.ResponseStart = currTime
+
+	// Response arrives
+	currTime = advanceTime(currTime, entry.Timings.Receive)
+	t.ResponseEnd = currTime
+}
+
 func parseFromHAR(col trace.Collector, log *hl.CustomHARLog) (int, sampled_err.Errors) {
 	// Use the same UUID for all witnesses from the same HAR file.
 	harUUID := uuid.New()
@@ -48,11 +111,17 @@ func parseFromHAR(col trace.Collector, log *hl.CustomHARLog) (int, sampled_err.E
 	for i, entry := range log.Entries {
 		entrySuccess := true
 
+		var ts ReconstructedTimestamps
+		ts.reconstructFromHAR(&entry)
 		if entry.Request != nil {
 			if req, err := convertRequest(entry.Request); err == nil {
 				req.StreamID = harUUID
 				req.Seq = i
-				col.Process(akinet.ParsedNetworkTraffic{ObservationTime: entry.StartedDateTime, Content: req})
+				col.Process(akinet.ParsedNetworkTraffic{
+					ObservationTime: ts.RequestStart,
+					FinalPacketTime: ts.RequestEnd,
+					Content:         req,
+				})
 			} else {
 				printer.Debugf("%s\n", errors.Wrapf(err, "failed to convert HAR request, index=%d", i))
 				entrySuccess = false
@@ -64,7 +133,11 @@ func parseFromHAR(col trace.Collector, log *hl.CustomHARLog) (int, sampled_err.E
 			if resp, err := convertResponse(entry.Response); err == nil {
 				resp.StreamID = harUUID
 				resp.Seq = i
-				col.Process(akinet.ParsedNetworkTraffic{ObservationTime: entry.StartedDateTime, Content: resp})
+				col.Process(akinet.ParsedNetworkTraffic{
+					ObservationTime: ts.ResponseStart,
+					FinalPacketTime: ts.ResponseEnd,
+					Content:         resp,
+				})
 			} else {
 				printer.Debugf("%s\n", errors.Wrapf(err, "failed to convert HAR response, index=%d", i))
 				entrySuccess = false
