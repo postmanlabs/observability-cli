a31c14f89a211926e21a313903a80843e51d9786
Cole Schlesinger <cole@akitasoftware.com>
2021-05-25 13:53:26 -0700

    Standardize help msg

--- cmd/internal/login/flags.go
+++ cmd/internal/login/flags.go
@@ -19,13 +19,13 @@ func registerOptionalFlags() {
 		&checkCreds,
 		"check",
 		false,
-		"If true, checks authentication with existing credentials rather than asking for new credentials.",
+		"check authentication with existing credentials rather than asking for new credentials",
 	)
 
 	Cmd.Flags().BoolVar(
 		&showCreds,
 		"show-creds",
 		false,
-		"If true, prints credentials to stdout.  Useful for debugging.",
+		"print credentials to stdout (useful for debugging)",
 	)
 }
