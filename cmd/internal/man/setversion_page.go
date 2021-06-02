23e8ff7094be4e093d05aa2288814618028de063
Jed Liu <liujed@users.noreply.github.com>
2021-06-01 19:43:52 -0700

    Address PR comment

--- cmd/internal/man/setversion_page.go
+++ cmd/internal/man/setversion_page.go
@@ -5,7 +5,7 @@ var setversionPage = `
 
 # Description
 
-Sets the version name of an API spec.
+Sets the version name of an API spec. Version names are unique within a service. For example, "latest" is a version name automatically assigned to the most recent API spec.
 
 # Examples
 
@@ -13,7 +13,7 @@ Sets the version name of an API spec.
 
 Marks the spec identified by <bt>akita://myservice:spec:beta<bt> as "v1.0.0" for <bt>my-service<bt>. Any spec in <bt>my-service<bt> that was previously marked with "v1.0.0" will no longer be associated with that version name.
 
-## akita setversion stable akita://my-service:spec:latest
+## akita setversion stable akita://my-service:spec:public
 
-Marks the spec identified by <bt>akita://myservice:spec:latest<bt> as "stable" for <bt>my-service<bt>. This removes the "stable" designation from all other specs in <bt>my-service<bt>.
+Marks the spec identified by <bt>akita://myservice:spec:public<bt> as "stable" for <bt>my-service<bt>. This removes the "stable" designation from all other specs in <bt>my-service<bt>.
 `
