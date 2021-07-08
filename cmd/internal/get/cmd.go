8b7f2c4d9ecce55811531b6a6de632e5d67c0145
Mark Gritter <mgritter@akitasoftware.com>
2021-07-07 20:26:33 -0500

    New CLI commands: List traces and models, and download models. (#65)
    * Command to list traces on the CLI.
    * Working list specs command.
    * Add download command, some work on command parsing
    * Add URI parsing to top-level get command
    * Review feedback: AND vs OR matching, defer file creation.
    * Comment fixes.

---
+++ cmd/internal/get/cmd.go
@@ -0,0 +1,39 @@
+package get
+
+import (
+	"fmt"
+
+	"github.com/akitasoftware/akita-libs/akiuri"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+)
+
+// Parent command for listing objects from Akita.
+var Cmd = &cobra.Command{
+	Use:          "get [AKITAURI]",
+	Short:        "List or download objects in the Akita cloud.",
+	Long:         "List or download objects in the Akita cloud.",
+	SilenceUsage: true,
+	RunE:         getByURIType,
+}
+
+func getByURIType(cmd *cobra.Command, args []string) error {
+	if len(args) == 0 {
+		return cmd.Help()
+	}
+	srcURI, err := akiuri.Parse(args[0])
+	if err != nil {
+		return errors.Wrapf(err, "%q is not an Akita URI", args[1])
+	}
+
+	switch {
+	case srcURI.ObjectType == nil:
+		return fmt.Errorf("Must specify a subcommand or an AkitaURI with a type.")
+	case srcURI.ObjectType.IsTrace():
+		return getTraces(cmd, args)
+	case srcURI.ObjectType.IsSpec():
+		return getSpecs(cmd, args)
+	default:
+		return fmt.Errorf("Unhandled URI type in %q", srcURI)
+	}
+}
