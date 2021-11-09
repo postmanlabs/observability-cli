b6306fccc2b4aafe8235a98fd37af1935496e610
Mark Gritter <mgritter@akitasoftware.com>
2021-11-08 18:52:33 -0600

    Add akitasoftware/plugin-flickr as the "flickr" plugin.

--- cmd/internal/pluginloader/precompiled_plugins.go
+++ cmd/internal/pluginloader/precompiled_plugins.go
@@ -2,10 +2,15 @@ package pluginloader
 
 import (
 	"github.com/akitasoftware/akita-cli/plugin"
+
+	"github.com/akitasoftware/plugin-flickr"
 )
 
 // To include a plugin in the Akita CLI build, import the Go package
 // above, and add a reference to its AkitaPluginLoader function here.
 var PrecompiledPlugins map[string]plugin.AkitaPluginLoader = map[string]plugin.AkitaPluginLoader{
 	// Example: "my_plugin" : myplugin.LoadAkitaPlugin,
+
+	// Plugin for transforming Flickr's API into a more REST-like format.
+	"flickr": plugin_flickr.LoadAkitaPlugin,
 }
