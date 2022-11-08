748e040aca490d3a92c1be1003ee88af915d87da
Cole Schlesinger <cole@akitasoftware.com>
2022-11-08 15:00:43 -0800

    Check unix.Uname-reported architecture

--- version/version.go
+++ version/version.go
@@ -1,11 +1,13 @@
 package version
 
 import (
+	"bytes"
 	"fmt"
 	"runtime"
 	"strings"
 
 	ver "github.com/hashicorp/go-version"
+	"golang.org/x/sys/unix"
 )
 
 var (
@@ -28,5 +30,14 @@ func GitVersion() string {
 }
 
 func CLIDisplayString() string {
-	return fmt.Sprintf("%s (%s, %s)", releaseVersion.String(), gitVersion, runtime.GOARCH)
+	var utsname unix.Utsname
+	_ = unix.Uname(&utsname)
+
+	archMsg := runtime.GOARCH
+	machineArch := string(bytes.Trim(utsname.Machine[:], "\x00"))
+	if runtime.GOARCH != machineArch {
+		archMsg = fmt.Sprintf("built for %s, running on %s", runtime.GOARCH, machineArch)
+	}
+
+	return fmt.Sprintf("%s (%s, %s)", releaseVersion.String(), gitVersion, archMsg)
 }
