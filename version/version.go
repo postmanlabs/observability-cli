c55650e6d5335f1996dcbbbe7fff1a9f8700679c
Cole Schlesinger <cole@akitasoftware.com>
2022-11-08 15:10:06 -0800

    Revert "Check unix.Uname-reported architecture"
    Unfortunately, unix.Uname reports the virtualized architecture, not the host
    architecture.
    
    This reverts commit 748e040aca490d3a92c1be1003ee88af915d87da.

--- version/version.go
+++ version/version.go
@@ -1,13 +1,11 @@
 package version
 
 import (
-	"bytes"
 	"fmt"
 	"runtime"
 	"strings"
 
 	ver "github.com/hashicorp/go-version"
-	"golang.org/x/sys/unix"
 )
 
 var (
@@ -30,14 +28,5 @@ func GitVersion() string {
 }
 
 func CLIDisplayString() string {
-	var utsname unix.Utsname
-	_ = unix.Uname(&utsname)
-
-	archMsg := runtime.GOARCH
-	machineArch := string(bytes.Trim(utsname.Machine[:], "\x00"))
-	if runtime.GOARCH != machineArch {
-		archMsg = fmt.Sprintf("built for %s, running on %s", runtime.GOARCH, machineArch)
-	}
-
-	return fmt.Sprintf("%s (%s, %s)", releaseVersion.String(), gitVersion, archMsg)
+	return fmt.Sprintf("%s (%s, %s)", releaseVersion.String(), gitVersion, runtime.GOARCH)
 }
