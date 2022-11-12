414ad5d2519336b1e6e546535ca77f74763b67cb
Cole Schlesinger <cole@akitasoftware.com>
2022-11-11 18:21:06 -0800

    Help when running in Docker Desktop
    Running the Akita agent in Docker Desktop (macOS or Windows) will fail to
    capture traffic to services running on the host.
    
    Rather than sharing the host networking stack, Docker Desktop runs the daemon
    in a VM.  Docker users are instructed to use host.internal.docker to
    communicate with the host.
    
    We can use the presence of that DNS name to detect if we're running in a
    container with Docker Desktop.  If so, suggest using the native macOS binary if
    the agent fails to capture traffic.

--- env/docker.go
+++ env/docker.go
@@ -1,6 +1,7 @@
 package env
 
 import (
+	"net"
 	"os"
 )
 
@@ -8,4 +9,15 @@ import (
 func InDocker() bool {
 	_, inDocker := os.LookupEnv("__X_AKITA_CLI_DOCKER")
 	return inDocker
+}
+
+const dockerInternalHostname = "host.docker.internal"
+
+// Docker exposes an internal address for containers to communicate with the
+// host.  It is only available on Docker Desktop and indicates the container
+// is being run by Docker Desktop on macOS or Windows.
+// https://docs.docker.com/desktop/networking/#i-want-to-connect-from-a-container-to-a-service-on-the-host
+func HasDockerInternalHostAddress() bool {
+	ips, err := net.LookupIP(dockerInternalHostname)
+	return err == nil && len(ips) > 0
 }
