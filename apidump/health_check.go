b102ba3600dc67f244e0857d0bfcb73c0c78b348
versilis <versilis@akitasoftware.com>
2022-11-14 22:49:46 -0600

    Fix router

--- apidump/health_check.go
+++ apidump/health_check.go
@@ -8,13 +8,15 @@ import (
 
 // Handles health check requests for the Docker Extension.
 // Returns 200 OK by default.
-func handleHealthCheck(w http.ResponseWriter, r *http.Request) {
+func handleHealthCheck(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	_, _ = w.Write([]byte(`{"status": "ok"}`))
 }
 
 func startHealthCheckServer(port int) error {
 	router := mux.NewRouter()
-	router.HandleFunc("/health", handleHealthCheck)
+
+	router.HandleFunc("/health", handleHealthCheck).Methods("GET")
+
 	return http.ListenAndServe(fmt.Sprintf(":%d", port), router)
 }
