e9bb6c2d72e4fad3630ffc07fccac85884317629
Jed Liu <liujed@users.noreply.github.com>
2024-03-07 18:18:44 -0800

    kube command: remove support for Akita authentication

--- cmd/internal/cmderr/checks.go
+++ cmd/internal/cmderr/checks.go
@@ -8,24 +8,6 @@ import (
 	"github.com/akitasoftware/akita-cli/printer"
 )
 
-// Checks that a user has configured their API key and secret and returned them.
-// If the user has not configured their API key, a user-friendly error message is printed and an error is returned.
-func RequireAkitaAPICredentials(explanation string) (string, string, error) {
-	key, secret := cfg.GetAPIKeyAndSecret()
-	if key == "" || secret == "" {
-		printer.Errorf("No Akita API key configured. %s\n", explanation)
-		if env.InDocker() {
-			printer.Infof("Please set the AKITA_API_KEY_ID and AKITA_API_KEY_SECRET environment variables on the Docker command line.\n")
-		} else {
-			printer.Infof("Use the AKITA_API_KEY_ID and AKITA_API_KEY_SECRET environment variables, or run 'akita login'.\n")
-		}
-
-		return "", "", AkitaErr{Err: errors.New("could not find an Akita API key to use")}
-	}
-
-	return key, secret, nil
-}
-
 // Checks that a user has configured their Postman API key and returned them.
 // If the user has not configured their API key, a user-friendly error message is printed and an error is returned.
 func RequirePostmanAPICredentials(explanation string) (string, error) {
